f9f8aa6e343e684c8b2cc44cac2dc7c57a499b7a
Chirag kapoor <chiragkapoor80@gmail.com>
2025-02-17 12:41:38 +0530

    Initialised app from the scratch

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const div = React.createElement('div',{id:'root'},
     React.createElement('div',{id:'parent'},
         [React.createElement('div',{id:'child'},[
             React.createElement('h1',{id:'grandchild'},"I am Grandchild 1 of child 1"),
-            React.createElement('h1',{id:"grandchild1"},'I am Grandchild 2 of child 1')
+            React.createElement('h1',{id:"grandchild1"},'Hello React I am Chirag')
         ]
         ),
         React.createElement('div',{id:'child2'},[
             React.createElement('h1',{id:'grandchild'},"I am Grandchild 1 of child 2"),
-            React.createElement('h1',{id:"grandchild1"},'I am Grandchild 2 of child 2')
+            React.createElement('h1',{id:"grandchild1"},'React is Beautiful Library')
         ]
         )]
     )
