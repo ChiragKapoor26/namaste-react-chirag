38a5692d22e44492f4f96ce644bf76d1f1e90560
Chirag kapoor <chiragkapoor80@gmail.com>
2025-04-07 15:13:09 +0530

    Filter Restaurants feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,21 +2,73 @@ import { useState } from "react";
 import Card from "./Card"
 import {restData} from "./utils/mock-data";
 const Body = () => {
+    const [listOfRestaurant,setlistOfRestaurant] = useState(restData.card.card.gridElements.infoWithStyle.restaurants);
+    // let listOfRestaurantJS = [
+    //     {
+    //         "info": {
+    //         "id": "234875",
+    //         "name": "Adil Hotel",
+    //         "cloudinaryImageId": "gp1ityra6utvzqn6ghnv",
+    //         "locality": "Rautha Wada",
+    //         "areaName": "Chhindwara Locality",
+    //         "costForTwo": "₹150 for two",
+    //         "cuisines": [
+    //         "North Indian",
+    //         "Biryani",
+    //         "Tandoor"
+    //         ],
+    //         "avgRating": 4.3,
+    //         }
+    //         },
+    //     {
+    //             "info": {
+    //             "id": "150591",
+    //             "name": "Satkar Restaurant",
+    //             "cloudinaryImageId": "rvxp5xbniat84r6efku2",
+    //             "locality": "Sinchai Colony",
+    //             "areaName": "Satkar Chowk",
+    //             "costForTwo": "₹250 for two",
+    //             "cuisines": [
+    //             "North Indian",
+    //             "South Indian",
+    //             "Indian",
+    //             "Salads",
+    //             "Desserts"
+    //             ],
+    //             "avgRating": 4.4,
+    //             }
+    //         },
+    //     {
+    //             "info": {
+    //             "id": "151649",
+    //             "name": "Hotel Sai Nath & Sai Restaurant",
+    //             "cloudinaryImageId": "vkhcohhmqfczycw9vsar",
+    //             "locality": "railway station",
+    //             "areaName": "Chhindwara Locality",
+    //             "costForTwo": "₹200 for two",
+    //             "cuisines": [
+    //             "North Indian",
+    //             "South Indian",
+    //             "Chinese",
+    //             "Beverages",
+    //             "Fast Food",
+    //             "Desserts"
+    //             ],
+    //             "avgRating": 4.3,
+    //             }
+    //         }
+    // ]
     return (
         <div className="body">
             <div className="search-container">
                 <input className="search-ip" placeholder="Order from here..."></input>
                 <button className="search-btn" onClick={() => {
-                    const resList = restData.card.card.gridElements.infoWithStyle.restaurants;
-                    resList = resList.filter((rest) => {
-                        return
-                        (rest>4.2)
-                    })
+                    setlistOfRestaurant(listOfRestaurant.filter((res)=>res.info.avgRating>4.1 ));
                 }}>Top Rated Restaurant</button>
             </div>
             <div className="rest-container">
                 {
-                    restData.card.card.gridElements.infoWithStyle.restaurants.map((restaurant) => (
+                    listOfRestaurant.map((restaurant) => (
                         <Card data={restaurant} key={restaurant.info.id}/>
                     ))
                 }
